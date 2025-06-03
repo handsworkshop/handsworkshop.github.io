624805b31b3080b258346ab72fc0c3792d5b7503
beikungg <2473910949@qq.com>
2025-06-03 16:15:10 +0800

    update 2025

---
+++ server.py
@@ -0,0 +1,14 @@
+import http.server
+import socketserver
+
+PORT = 8016
+
+Handler = http.server.SimpleHTTPRequestHandler
+Handler.extensions_map.update({
+    '.html': 'text/html',
+})
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"服务器运行在 http://localhost:{PORT}")
+    print("按 Ctrl+C 停止服务器")
+    httpd.serve_forever() 
